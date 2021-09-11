fc24b87dc54ea25817b544877381a0dd4d918c6e
lucas0liveira <lucasoliveira96@outlook.com>
2021-09-11 15:35:40 -0400

    início do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="app">
+        hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
